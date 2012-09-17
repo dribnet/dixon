ac2016eeedbbdc78d266e76a4e926c630a4fea92
Tom White <tom@sixdozen.com>
2012-09-17 04:58:42 -0700

    viz: grid now animating
    Added animation loop.

--- src/cljs/dixon/core.cljs
+++ src/cljs/dixon/core.cljs
@@ -3,24 +3,23 @@
   (:use [c2.core :only [unify style]])
   (:require [c2.scale :as scale]))
 
-#_(bind! "#board" 
-  [:svg#main {:style (str "display: block;"
-                          "margin: auto;"
-                          "height:" 700 ";"
-                          "width:" 700 ";")}
-    [:rect.box {:x 500 :y 400 :height 24 :width 24
-                :fill "#05182F" :rx 3 :style {:opacity 1}}]
-    [:rect.box {:x 520 :y 420 :height 24 :width 24
-                :fill "#05182F" :rx 3 :style {:opacity 1}}]
-                                ])
+(defn docycle [counter]
+  (bind! "#board" 
+    [:svg {:style (str "display: block;"
+                            "margin: auto;"
+                            "height:" 700 ";"
+                            "width:" 700 ";")}
+    (unify (for [x (range 35) y (range 35) :when (= (mod x 5) (mod counter 5))]
+              [x y])
+        (fn [[x y]]
+          [:rect.box {:x (* 20 x) :y (* 20 y) :height 24 :width 24
+                  :fill "#05182F" :rx 3 :style {:opacity 1}}]))
+                                  ]))
 
-(bind! "#board" 
-  [:svg#main {:style (str "display: block;"
-                          "margin: auto;"
-                          "height:" 700 ";"
-                          "width:" 700 ";")}
-  (unify (for [x (range 35) y (range 35) :when (even? (+ x y))] [x y])
-      (fn [[x y]]
-        [:rect.box {:x (* 20 x) :y (* 20 y) :height 24 :width 24
-                :fill "#05182F" :rx 3 :style {:opacity 1}}]))
-                                ])
+(def count-atom (atom 0))
+
+(defn nextloop []
+  (swap! count-atom inc)
+  (docycle @count-atom))
+
+(js/setInterval #(nextloop) (/ 1000 30))
