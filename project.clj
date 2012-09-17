71ea4ab15b3fcaf2ba4316eb38bcdd233f955f92
Tom White <tom@sixdozen.com>
2012-09-17 03:41:40 -0700

    viz: added c2 scaffolding

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
 (defproject dixon "0.0.1-SNAPSHOT"
+  :description "dixon: dots on a screen"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1450"]
+                 [com.keminglabs/c2 "0.2.0"]
                 ]
-  :plugins [[lein-cljsbuild "0.2.7"]]
+  :min-lein-version "2.0.0"
   :source-paths ["src/clj"]
-  :description "dixon: dots on a screen"
   :url "http://example.com/FIXME"
+
+  :plugins [[lein-cljsbuild "0.2.7"]]
+
   :cljsbuild {
     :builds [{
       :source-path "src/cljs"
       :compiler {
         :output-to "web/out/dixon.js"
         :optimizations :whitespace
-        :pretty-print true
-      }
-    }]
-  }
-)
+        :pretty-print true }}]})
--- src/cljs/dixon/core.cljs
+++ src/cljs/dixon/core.cljs
@@ -1,2 +1,18 @@
-(ns dixon.core)
-(.write js/document "<p>Hello, dixon</p>")
+(ns dixon.core
+  (:use-macros [c2.util :only [p pp bind!]])
+  (:use [c2.core :only [unify style]])
+  (:require [c2.scale :as scale]))
+
+(bind! "#bars" 
+  (let [width 500 bar-height 20
+        data {"A" 1, "B" 2, "C" 4, "D" 3}
+        s (scale/linear :domain [0 (apply max (vals data))]
+                        :range [0 width])]
+
+    [:div
+     (unify data (fn [[label val]]
+                   [:div {:style {:height bar-height
+                                  :width (s val)
+                                  :background-color "gray"}}
+                      [:span {:style {:color "white"}} label]]))]))
+
