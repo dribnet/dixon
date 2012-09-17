5329da3ef2b95117c3b6abe77afeb547d693d314
Tom White <tom@sixdozen.com>
2012-09-17 04:35:46 -0700

    viz: for comprehension and unify to draw checkboard
    many rects instead of just one. should be close
    to a game of life.

--- src/cljs/dixon/core.cljs
+++ src/cljs/dixon/core.cljs
@@ -4,24 +4,23 @@
   (:require [c2.scale :as scale]))
 
 #_(bind! "#board" 
-  (let [width 500 bar-height 20
-        data {"A" 1, "B" 2, "C" 4, "D" 3}
-        s (scale/linear :domain [0 (apply max (vals data))]
-                        :range [0 width])]
-
-    [:div
-     (unify data (fn [[label val]]
-                   [:div {:style {:height bar-height
-                                  :width (s val)
-                                  :background-color "gray"}}
-                      [:span {:style {:color "white"}} label]]))]))
-
+  [:svg#main {:style (str "display: block;"
+                          "margin: auto;"
+                          "height:" 700 ";"
+                          "width:" 700 ";")}
+    [:rect.box {:x 500 :y 400 :height 24 :width 24
+                :fill "#05182F" :rx 3 :style {:opacity 1}}]
+    [:rect.box {:x 520 :y 420 :height 24 :width 24
+                :fill "#05182F" :rx 3 :style {:opacity 1}}]
+                                ])
 
 (bind! "#board" 
   [:svg#main {:style (str "display: block;"
                           "margin: auto;"
                           "height:" 700 ";"
                           "width:" 700 ";")}
-    [:rect.box {:x 672 :y 464 :height 24 :width 24
-                :fill "#05182F" :rx 3 :style {:opacity 0}}]])
- 
+  (unify (for [x (range 35) y (range 35) :when (even? (+ x y))] [x y])
+      (fn [[x y]]
+        [:rect.box {:x (* 20 x) :y (* 20 y) :height 24 :width 24
+                :fill "#05182F" :rx 3 :style {:opacity 1}}]))
+                                ])
