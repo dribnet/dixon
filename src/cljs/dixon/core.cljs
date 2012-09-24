4b9f290ebdf37292a1b4b73faaf138dcace745f7
Tom White <tom@sixdozen.com>
2012-09-24 02:59:54 -0700

    viz: new display atom fixes unify binding
    Found bug: the binding isn't going to update
    if the 1st unify argument is always the same.
    So we just make a dedicated unify data structure
    for now.
    
    Also added randomization to the beginning and tweaked
    css styling.

--- src/cljs/dixon/core.cljs
+++ src/cljs/dixon/core.cljs
@@ -16,35 +16,65 @@
   [:require [c2.scale :as scale]
             [goog.dom :as dom]])
 
+(def grid-width 40)
+(def grid-height 30)
+
+(def display-atom (atom {}))
+
 (defn docycle [grid w h onfunc]
   (bind! "#board" 
     [:svg {:style (str "display: block;"
                        "margin: auto;"
                        "height:" 700 ";"
                        "width:" 700 ";")}
-    (unify (for [x (range w) y (range h) :when (not= nil (onfunc grid y x))]
-              [x y])
-        (fn [[x y]]
-          (let [css-class (if (onfunc grid y x) "box-on" "box-off")]
+    (unify @display-atom
+       (fn [{:keys [x y state]}]
+          (let [css-class (if state "box-on" "box-off")]
             [:rect {:x (* 20 x) :y (* 20 y) :height 24 :width 24
                   :class css-class :rx 3}] )))
                                   ]))
 
-(def grid (populate-grid (empty-grid 10 10) #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
+; (defn update-display-atom []
+;   (let [pairs (for [x (range 10) y (range 10)] [x y])]
+
+(def grid (populate-grid (empty-grid grid-width grid-height) #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
 
 (def grid-atom (atom grid))
+
+(dotimes [_ 1000]
+  (let [x (rand-int grid-width)
+        y (rand-int grid-height)]
+    (reset! grid-atom (populate-grid @grid-atom #{[x y]}))))
 
 (defn grid-is-on [grid y x]
   (nth (nth grid y) x))
 
+; gross
+; (defn update-display-atom []
+;   (loop [board [] x 0]
+;     (loop [boardy board y 0]
+;       (if (= y 10)
+;         boardy
+;         (recur (conj boardy {:x x :y y :state (grid-is-on @grid-atom x y)}) (inc y))))
+;     (if (= x 10)
+;       board
+;       (recur board (inc x)))))
+
+; map grid into a list of maps that can be fed to unify
+(defn update-display-atom []
+  (for [x (range grid-width) y (range grid-height)] {:x x :y y :state (grid-is-on @grid-atom x y)}))
+
 ;(docycle grid 35 35 grid-is-on)
+
+(docycle grid-atom grid-width grid-height grid-is-on)
 
 (defn nextloop []
   (swap! grid-atom grid-step)
-  (docycle @grid-atom 10 10 grid-is-on))
+  (reset! display-atom (update-display-atom)))
 
 (defn animation-loop []
   (.requestAnimationFrame (dom/getWindow) animation-loop)
+  ;(.log js/console (str "Looping..." (grid-is-on @grid-atom 9 9)))
   (nextloop))
 
 (animation-loop)
